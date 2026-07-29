891c7f45d5f249d08f9c5ae2ef1d2aaca7538393
dr <draytchev@gmail.com>
2026-07-29 11:37:20 +0300

    updated envs

--- src/config/config.ts
+++ src/config/config.ts
@@ -11,6 +11,5 @@ export const config = {
   mongoPort: parseInt(process.env.MONGO_DB_PORT ?? "27017", 10),
   mongoDb: process.env.MONGO_DB_TABLE ?? "powerbi-ip-whitelist",
 
-  // Whether to remove stale rules that are no longer in the published list
   pruneStaleRules: process.env.PRUNE_STALE_RULES === "true",
 };
